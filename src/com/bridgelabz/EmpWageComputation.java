9db6615777cb162506270156d403a4a6b2ad5d90
VinitGrover <vinitgrover255@gmail.com>
2022-05-29 16:03:58 +0530

    Added uc7

--- src/com/bridgelabz/EmpWageComputation.java
+++ src/com/bridgelabz/EmpWageComputation.java
@@ -1,7 +1,40 @@
 package com.bridgelabz;
+import java.util.*;
 
-public class EmpWageComputation {
+class EmpWageComputation {
+
+    void totalWage(int i, int maxHour) {
+
+        Random randNum = new Random();
+        int j = 0, hour = 0, sum = 0, maximumHour = 0;
+
+        while (j < i && maximumHour < maxHour) {
+
+            int check = randNum.nextInt(3);
+
+            switch (check) {
+                case 0:
+                    hour = 0;
+                    break;
+                case 1:
+                    hour = 4;
+                    break;
+                default:
+                    hour = 8;
+            }
+            System.out.println("daily Wages are : " + hour * 20);
+            sum += hour * 20;
+            j++;
+            maxHour += hour;
+        }
+        System.out.println("Total Wages in 20 day period " + sum);
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
+        System.out.println("Welcome to Employee Wage Computation program on Master Branch");
+        EmpWageComputation emp1 = new EmpWageComputation();
+        emp1.totalWage(20, 100);
     }
 }
+
