67f7b4c7dc597d4174248b865ae35dc3c3ac239a
VinitGrover <vinitgrover255@gmail.com>
2022-06-02 22:55:36 +0530

    Add uc11

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -1,22 +1,8 @@
 package com.bridgelabz;
 
-public class EmpWageBuilder extends EmpWageComputation {
+interface EmpWageBuilder {
 
-    void empWage() {
-        System.out.println("Welcome to Employee Wage Computation program ");
+    void empWage(EmpWageComputation m1);
 
-        int maxHour, maxDays, wagePerHour;
-
-        System.out.print("Enter Maximum working hours : ");
-        maxHour = EmpWageComputation.sc.nextInt();
-
-        System.out.print("Enter Maximum working days : ");
-        maxDays = EmpWageComputation.sc.nextInt();
-
-        System.out.print("Enter Employee Wage per hour : ");
-        wagePerHour = EmpWageComputation.sc.nextInt();
-
-        totalWage(maxDays, maxHour, wagePerHour);
-
-    }
 }
+
--- src/com/bridgelabz/MultilpleCompany.java
+++ src/com/bridgelabz/MultilpleCompany.java
@@ -2,12 +2,33 @@ package com.bridgelabz;
 
 import java.util.ArrayList;
 
-public class MultilpleCompany {
+public class MultilpleCompany implements EmpWageBuilder {
+
+    public void empWage(EmpWageComputation m1){
+        System.out.println("Welcome to Employee Wage Computation program ");
+
+        int maxHour, maxDays, wagePerHour;
+
+        System.out.print("Enter Maximum working hours : ");
+        maxHour = EmpWageComputation.sc.nextInt();
+
+        System.out.print("Enter Maximum working days : ");
+        maxDays = EmpWageComputation.sc.nextInt();
+
+        System.out.print("Enter Employee Wage per hour : ");
+        wagePerHour = EmpWageComputation.sc.nextInt();
+
+        m1.totalWage(maxDays, maxHour, wagePerHour);
+
+    }
+
     public static void main(String[] args) {
-        EmpWageBuilder e1 = new EmpWageBuilder();
-        e1.empWage();
+
+        EmpWageComputation m1 = new EmpWageComputation();
+        MultilpleCompany c1 = new MultilpleCompany();
+        c1.empWage(m1);
 
         ArrayList<EmpWageBuilder> a1 = new ArrayList();
-        a1.add(e1);
+        a1.add(c1);
     }
 }
